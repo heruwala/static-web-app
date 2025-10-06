dd0c72131c0ca81c97d9713ee16427140c6feccb
Dhaval Heruwala <heruwala@gmail.com>
2025-10-05 22:48:29 -0400

    Refactor App component to update welcome message and remove unused elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My Homepage</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is my new Web App!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
